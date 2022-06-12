5f4f0cff1bf3438acd5f2344c877ecc27502981a
ChenL <chenL@sun.com>
2022-06-12 16:08:32 +0800

    third commit

--- src/main/java/com/xmut/git/TodayGit.java
+++ src/main/java/com/xmut/git/TodayGit.java
@@ -13,5 +13,6 @@ public class TodayGit {
         System.out.println("2022 06 12 hello git ");
 
         System.out.println("2022 06 12 hello git2 ");
+        System.out.println("2022 06 12 hello git3 ");
     }
 }

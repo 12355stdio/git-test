28ca9f93e8810da78210ca2564eab82b98406395
ChenL <chenL@sun.com>
2022-06-12 16:01:37 +0800

    first commit

---
+++ src/main/java/com/xmut/git/TodayGit.java
@@ -0,0 +1,15 @@
+package com.xmut.git;/*
+ * @Author chenL
+ * @Date 2022/6/12 15:55
+ * @PackageName:com.xmut.git
+ * @ClassName: TodayGit
+ * @Description:
+ * @Version 1.0
+ * */
+
+public class TodayGit {
+    public static void main(String[] args) {
+
+        System.out.println("2022 06 12 hello git ");
+    }
+}
